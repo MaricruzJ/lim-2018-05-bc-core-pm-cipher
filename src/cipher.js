38a73dd9900b44cf92fb59a4eb80dedbcd00ba5b
MaricruzJ <maricruzj.er@gmail.com>
2018-05-25 15:49:09 -0500

    Actualizacion Semana 1 - Proyecto 1

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,28 @@
+const txtInit = document.getElementById("textoInicial");
+const saltos = document.getElementById("saltos");
+const txtEnd = document.getElementById("textoModificado");
+
+const cifrar = () => {
+  for( let i= 0; i < txtInit.value.length; i++){
+    let posiciontextoInicial = txtInit.value.toUpperCase().charCodeAt(i);
+    console.log(posiciontextoInicial);
+    let posicionCifrado = (posiciontextoInicial - 65 + parseInt(saltos.value)) % 26 + 65;
+    let letracifrada = String.fromCharCode(posicionCifrado);
+    txtEnd.value += letracifrada;
+  }
+}
+
+const descifrar = () => {
+  for( let i= 0; i < txtInit.value.length; i++){
+    let posiciontextoInicial = txtInit.value.toUpperCase().charCodeAt(i);
+    let posicionDescifrado = (posiciontextoInicial + 65 - parseInt(saltos.value)) % 26 + 65;
+    let letraDescifrada = String.fromCharCode(posicionDescifrado);
+    txtEnd.value += letraDescifrada;
+  }
+}
+
 window.cipher = {
-  // ... 
+  encode : cifrar,
+  decode : descifrar
 };
+ 
---
+++ src/index.js
@@ -0,0 +1,5 @@
+var btncifrar = document.getElementById('btnCifrado');
+btncifrar.addEventListener('click',window.cipher.encode);
+
+var btndescifrar = document.getElementById('btnDescifrado');
+btndescifrar.addEventListener('click',window.cipher.decode);
