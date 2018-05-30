33e2b68d5e401bf0f6ef271f9ce628f29514ffee
MaricruzJ <maricruzj.er@gmail.com>
2018-05-30 18:27:24 -0500

    Actualización Eventos del DOM (index.js) y cipher.js Test 0 errores

--- src/cipher.js
+++ src/cipher.js
@@ -1,84 +1,69 @@
-const txtInit = document.getElementById('textoInicial');
-const saltos = document.getElementById('saltos');
-const txtEnd = document.getElementById('textoModificado');
+let posicionCifrado;
+let letracifrada;
+let nuevaPosicion;
+let posicionDescifrado;
+let letraDescifrada;
 
-const cifrar = () => {
-  txtEnd.value = '';
-  
-  for( let i= 0; i < txtInit.value.length; i++){
+const cifrar = (txtInit,saltos) => {
+  let txtEnd = '';
+ 
+  for( let i= 0; i < txtInit.length; i++){
     
-    let posiciontextoInicial = txtInit.value.charCodeAt(i);
-    
+    let posiciontextoInicial = txtInit.charCodeAt(i);
+
     if(posiciontextoInicial >= 65 && posiciontextoInicial <= 90){
-      let posicionCifrado = (posiciontextoInicial - 65 + parseInt(saltos.value)) % 26 + 65;
-      let letracifrada = String.fromCharCode(posicionCifrado);
-      txtEnd.value += letracifrada;
-    }else if (posiciontextoInicial >= 97 && posiciontextoInicial <= 122){
-      posicionCifrado = (posiciontextoInicial - 97 + parseInt(saltos.value)) % 26 + 97;
+      posicionCifrado = (posiciontextoInicial - 65 + parseInt(saltos)) % 26 + 65;
       letracifrada = String.fromCharCode(posicionCifrado);
-      txtEnd.value += letracifrada;
+      txtEnd += letracifrada;
+
+    }else if (posiciontextoInicial >= 97 && posiciontextoInicial <= 122){
+      posicionCifrado = (posiciontextoInicial - 97 + parseInt(saltos)) % 26 + 97;
+      letracifrada = String.fromCharCode(posicionCifrado);
+      txtEnd += letracifrada;
       
-    //}else if(posiciontextoInicial >= 32 && posiciontextoInicial <= 64){
+    
     } else {
       letracifrada = String.fromCharCode(posiciontextoInicial);
-      txtEnd.value += letracifrada;
+      txtEnd += letracifrada;
     }
   }
+  return txtEnd;
 }
 
-const descifrar = () => {
-  txtEnd.value = '';
+const descifrar = (txtInit,saltos) => {
+ let  txtEnd = '';
   
-  for( let i= 0; i < txtInit.value.length; i++){
+  for( let i= 0; i < txtInit.length; i++){
     
-    let posiciontextoInicial = txtInit.value.charCodeAt(i);
+    let posiciontextoInicial = txtInit.charCodeAt(i);
     
     if(posiciontextoInicial >= 65 && posiciontextoInicial <= 90){
-      let posicionDescifrado = (posiciontextoInicial + 65 - parseInt(saltos.value)) % 26 + 65;
-      let letraDescifrada = String.fromCharCode(posicionDescifrado);
-      txtEnd.value += letraDescifrada;
+      posicionDescifrado = (posiciontextoInicial + 65 - parseInt(saltos)) % 26 + 65;
+      letraDescifrada = String.fromCharCode(posicionDescifrado);
+      txtEnd += letraDescifrada;
 
     }else if (posiciontextoInicial >= 97 && posiciontextoInicial <= 122){
-      let nuevaPosicion = posiciontextoInicial-32;
-      posicionDescifrado = (nuevaPosicion + 65 - parseInt(saltos.value)) % 26 + 65;
+      nuevaPosicion = posiciontextoInicial-32;
+      posicionDescifrado = (nuevaPosicion + 65 - parseInt(saltos)) % 26 + 65;
       letraDescifrada = String.fromCharCode(posicionDescifrado);
-      txtEnd.value += letraDescifrada.toLowerCase();
+      txtEnd += letraDescifrada.toLowerCase();
 
     }else if(posiciontextoInicial >= 32 && posiciontextoInicial <= 64){
-      posicionCifrado = posiciontextoInicial;
-      letracifrada = String.fromCharCode(posicionCifrado);
-      txtEnd.value += letracifrada;
+      posicionDescifrado = posiciontextoInicial;
+      letraDescifrada = String.fromCharCode(posicionDescifrado);
+      txtEnd += letraDescifrada;
     }
-  }
+  } 
+  return txtEnd;
 }
-
-
-
-
-/* const cifrar = () => {
-txtEnd.value = '';
-for( let i= 0; i < txtInit.value.length; i++){
-let posiciontextoInicial = txtInit.value.toUpperCase().charCodeAt(i);
-console.log(posiciontextoInicial);
-let posicionCifrado = (posiciontextoInicial - 65 + parseInt(saltos.value)) % 26 + 65;
-let letracifrada = String.fromCharCode(posicionCifrado);
-txtEnd.value += letracifrada;
-}
-}
-
-const descifrar = () => {
-txtEnd.value = '';
-for( let i= 0; i < txtInit.value.length; i++){
-let posiciontextoInicial = txtInit.value.toUpperCase().charCodeAt(i);
-let posicionDescifrado = (posiciontextoInicial + 65 - parseInt(saltos.value)) % 26 + 65;
-let letraDescifrada = String.fromCharCode(posicionDescifrado);
-txtEnd.value += letraDescifrada;
-}
-} */
-
-
 
 window.cipher = {
-  encode : cifrar,
-  decode : descifrar
+  encode : (string,offset) => {
+    const result = cifrar(string,offset);
+    return result;
+  },
+  decode : (string,offset) => {
+    const result = descifrar(string,offset);
+    return result;
+  },
 };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
+const txtInit = document.getElementById('textoInicial');
+const saltos = document.getElementById('saltos');
+const txtEnd = document.getElementById('textoModificado');
+
 var btncifrar = document.getElementById('btnCifrado');
-btncifrar.addEventListener('click',window.cipher.encode);
+btncifrar.addEventListener('click', () => {
+    //cifrar(txtInit.value,saltos.value);
+    txtEnd.value = window.cipher.encode(txtInit.value,saltos.value);
+});
 
 var btndescifrar = document.getElementById('btnDescifrado');
-btndescifrar.addEventListener('click',window.cipher.decode);
+btndescifrar.addEventListener('click', () => {
+    //descifrar(txtInit.value, saltos.value);
+    txtEnd.value = window.cipher.decode(txtInit.value,saltos.value);
+} );
